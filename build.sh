2939056dc20eb11cece36fe50451e4a34a6f486c
iksdick <102718951+iksdick@users.noreply.github.com>
2025-03-28 06:04:40 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -o errexit
 
-# Install Tesseract and Japanese language data
+# Install system dependencies
 sudo apt-get update
-sudo apt-get install -y tesseract-ocr libtesseract-dev tesseract-ocr-jpn tesseract-ocr-script-jpan-vert
+sudo apt-get install -y \
+    tesseract-ocr \
+    libtesseract-dev \
+    tesseract-ocr-jpn \
+    tesseract-ocr-script-jpan-vert \
+    mecab \
+    mecab-ipadic-utf8 \
+    libmecab-dev
 
 # Install Python dependencies
 pip install -r requirements.txt
