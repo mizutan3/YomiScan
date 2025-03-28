805417bb71dd80b3e2bae057527b48a3254406b6
iksdick <102718951+iksdick@users.noreply.github.com>
2025-03-28 06:16:10 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
 set -o errexit
 
-# Install system dependencies
-sudo apt-get update
+# Install MeCab first
+chmod +x install_mecab.sh
+./install_mecab.sh
+
+# Then install OCR tools
 sudo apt-get install -y \
     tesseract-ocr \
     libtesseract-dev \
     tesseract-ocr-jpn \
-    tesseract-ocr-script-jpan-vert \
-    mecab \
-    mecab-ipadic-utf8 \
-    libmecab-dev \
-    git \
-    make \
-    curl \
-    xz-utils \
-    file
-
-# Install MeCab IPA dictionary (more reliable)
-sudo mkdir -p /usr/local/etc/
-curl -s https://raw.githubusercontent.com/neologd/mecab-ipadic-neologd/master/seed/mecab-user-dict-seed.20200910.csv.xz | xz -dc | iconv -f utf8 -t eucjp | /usr/lib/mecab/mecab-dict-index -d /usr/share/mecab/dic/ipadic -u /usr/local/etc/mecab-user-dict.dic -f eucjp -t eucjp
-echo "dicdir = /usr/share/mecab/dic/ipadic" | sudo tee /usr/local/etc/mecabrc
-echo "userdic = /usr/local/etc/mecab-user-dict.dic" | sudo tee -a /usr/local/etc/mecabrc
-
-# Verify installation
-mecab --version
-mecab-config --dicdir
+    tesseract-ocr-script-jpan-vert
 
 # Install Python dependencies
 pip install -r requirements.txt
+
+# Copy unidic-lite dictionary to expected location
+UNIDIC_PATH=$(python -c "import unidic_lite; print(unidic_lite.__path__[0])")
+sudo cp -r $UNIDIC_PATH/dicdir /usr/local/lib/mecab/dic/unidic-lite
