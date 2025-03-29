51c82090e708241d697c879e452ab4cd7feb2bfd
iksdick <102718951+iksdick@users.noreply.github.com>
2025-03-29 08:15:41 +0200

    Delete setup_tessdata.sh

--- setup_tessdata.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p /app/data/tessdata
-cd /app/data/tessdata
-wget https://github.com/tesseract-ocr/tessdata/raw/main/jpn.traineddata
-wget https://github.com/tesseract-ocr/tessdata/raw/main/jpn_vert.traineddata
