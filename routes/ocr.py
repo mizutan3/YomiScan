fda82a5e1165cca9e08b33eb67ae5a73edc0335a
iksdick <102718951+iksdick@users.noreply.github.com>
2025-04-25 10:59:41 +0300

    Delete routes directory

--- routes/ocr.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request
-from ..services.ocr_service import ocr
-
-ocr_bp = Blueprint("ocr", __name__)
-
-@ocr_bp.route("/ocr", methods=["POST"])
-def ocr_route():
-    return ocr()
