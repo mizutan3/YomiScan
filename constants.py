81b3d5de5d9b9f16d5e434c01871b04b0cd5eed5
iksdick <102718951+iksdick@users.noreply.github.com>
2025-04-25 09:42:08 +0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,17 @@
+import os
+from typing import Dict, List, Set
+
+TEMP_IMAGE_PATH = "./temp_image.jpg"
+
+#DICTIONARY_BASE_PATH = os.environ.get("DICTIONARY_PATH", "./dictionaries")
+#USER_DATA_DIR = os.environ.get("USER_DATA_PATH", "./user_data")
+
+DICTIONARY_BASE_PATH = os.path.join(os.path.dirname(__file__), "dictionaries")
+USER_DATA_DIR = os.path.join(os.path.dirname(__file__), "user_data")
+
+os.makedirs(DICTIONARY_BASE_PATH, exist_ok=True)
+os.makedirs(USER_DATA_DIR, exist_ok=True)
+
+dictionary_map: Dict[str, Dict[str, Dict[str, List[Dict]]]] = {}
+active_dictionaries: Set[str] = set()
+dictionary_order: List[str] = []
