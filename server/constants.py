040f2470e564bc4c21550a3bad5825995eebcc06
iksdick <102718951+iksdick@users.noreply.github.com>
2025-04-27 17:36:20 +0300

    Update constants.py

--- server/constants.py
+++ server/constants.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Set
 TEMP_IMAGE_PATH = "./temp_image.jpg"
 
 DICTIONARY_BASE_PATH = os.environ.get("DICTIONARY_PATH", "./dictionaries")
-USER_DATA_DIR = os.environ.get("USER_DATA_PATH", "./user_data")
+USER_DATA_DIR = os.environ.get("USER_DATA_PATH", "/app/data/user_data")
 TESSDATA_DIR = os.environ.get("TESSDATA_PATH", "./tessdata")
 
 #DICTIONARY_BASE_PATH = os.path.join(os.path.dirname(__file__), "dictionaries")
